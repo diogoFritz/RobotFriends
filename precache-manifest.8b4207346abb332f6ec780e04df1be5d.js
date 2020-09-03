26e713c11dbbbe50bfe6f996e9f2d5dc65308b2e
Diogo Freitas <diogofreitas1994@gmail.com>
2020-09-03 19:37:52 +0100

    Updates

---
+++ precache-manifest.8b4207346abb332f6ec780e04df1be5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a7fc6bce4cdb88deb2a4bd6ddddf169",
+    "url": "/diogoFritz/RobotFriends/index.html"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/diogoFritz/RobotFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f8b6c36baca675798c8a",
+    "url": "/diogoFritz/RobotFriends/static/css/main.bdeecae2.chunk.css"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/diogoFritz/RobotFriends/static/js/2.b52811a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diogoFritz/RobotFriends/static/js/2.b52811a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b6c36baca675798c8a",
+    "url": "/diogoFritz/RobotFriends/static/js/main.36839f59.chunk.js"
+  },
+  {
+    "revision": "7331f2dcc9b998fdb3de",
+    "url": "/diogoFritz/RobotFriends/static/js/runtime-main.b6e30209.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/diogoFritz/RobotFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
