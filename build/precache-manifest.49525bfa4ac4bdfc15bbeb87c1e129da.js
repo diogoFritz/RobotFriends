cac6405f84ab6597098b5eaf3ce3470c1d84fe21
Diogo Freitas <diogofreitas1994@gmail.com>
2020-09-03 21:16:05 +0100

    add builder folder

---
+++ build/precache-manifest.49525bfa4ac4bdfc15bbeb87c1e129da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605380ce94ab34dff6ed57bc983e7749",
+    "url": "/diogoFritz/RobotFriends/index.html"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/diogoFritz/RobotFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "51024274bc618f27c785",
+    "url": "/diogoFritz/RobotFriends/static/css/main.bdeecae2.chunk.css"
+  },
+  {
+    "revision": "7d65e0d887d1a91fcd1f",
+    "url": "/diogoFritz/RobotFriends/static/js/2.b52811a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diogoFritz/RobotFriends/static/js/2.b52811a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51024274bc618f27c785",
+    "url": "/diogoFritz/RobotFriends/static/js/main.90012f9e.chunk.js"
+  },
+  {
+    "revision": "7331f2dcc9b998fdb3de",
+    "url": "/diogoFritz/RobotFriends/static/js/runtime-main.b6e30209.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/diogoFritz/RobotFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
